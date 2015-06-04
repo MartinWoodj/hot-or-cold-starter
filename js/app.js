cf9ceec5e1069b2428bfba56407190c2b7298c94
Joseph Wood <martinwoodj@me.com>
2015-06-04 14:56:56 -0400

    Working on capturing User Guess

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
-
 $(document).ready(function(){
 	
-	/*--- Display information modal box ---*/
+  /*--- Generate random number when page loads ---*/
+  var randomNumber = Math.floor(Math.random()* 101);
+    console.log("Page Generated " + randomNumber);  
+  
+  /*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
 
@@ -11,7 +14,24 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-
+  	
+  	/*--- Generate random number when +New Game is clicked ---*/
+  	$(".new").click(function(){
+	  	randomNumber = Math.floor(Math.random()* 101);
+	  	console.log("Clicked New Game " + randomNumber);
+	  	console.log(typeof randomNumber);
+  	});
+  
+  /*--- Prevent submit button from generating a new number ---*/
+  $("#guessButton").submit(function(e){
+    return false;
+  });
+  	
+  	/*--- Compare random guess with random number ---*/
+  $("input").submit(function(){	
+  var userGuess = $("#userGuess").val();
+    console.log("User Guess " + userGuess);
+  });
 });
 
 
