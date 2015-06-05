bf86021894d30e3d68a46d6e58216141f21d32a1
Joseph Wood <martinwoodj@me.com>
2015-06-04 21:18:38 -0400

    Editing User Guess Function

--- js/app.js
+++ js/app.js
@@ -1,37 +1,42 @@
 $(document).ready(function(){
 	
-  /*--- Generate random number when page loads ---*/
-  var randomNumber = Math.floor(Math.random()* 101);
-    console.log("Page Generated " + randomNumber);  
-  
-  /*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	  /*--- Generate random number when page loads ---*/
+	  var randomNumber = Math.floor(Math.random()* 101);
+	    console.log("Page Generated " + randomNumber);  
+	  
+	  /*--- Display information modal box ---*/
+	  	$(".what").click(function(){
+	    	$(".overlay").fadeIn(1000);
+	  	});
+	
+	  	/*--- Hide information modal box ---*/
+	  	$("a.close").click(function(){
+	  		$(".overlay").fadeOut(1000);
+	  	});
+	  	
+	  	/*--- Generate random number when +New Game is clicked ---*/
+	  	$(".new").click(function(){
+		  	randomNumber = Math.floor(Math.random()* 101);
+		  	console.log("Clicked New Game " + randomNumber);
+		  	console.log(typeof randomNumber);
+	  	});
+	  
+	  /*--- Prevent submit button from generating a new number 
+		$( "#guessButton" ).submit(function(e) {
+		  console.log( "Handler for .submit() called." );
+		  e.preventDefault();
+		});---*/
+	  	
+	  	/*--- Compare random guess with random number ---*/
+		$("#guessButton").submit(function(e){	
+		  userGuess = $('#userGuess').val();
+		  console.log("User Guess " + userGuess);
+		});
 
-  	});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-  	
-  	/*--- Generate random number when +New Game is clicked ---*/
-  	$(".new").click(function(){
-	  	randomNumber = Math.floor(Math.random()* 101);
-	  	console.log("Clicked New Game " + randomNumber);
-	  	console.log(typeof randomNumber);
-  	});
-  
-  /*--- Prevent submit button from generating a new number ---*/
-  $("#guessButton").submit(function(e){
-    return false;
-  });
-  	
-  	/*--- Compare random guess with random number ---*/
-  $("input").submit(function(){	
-  var userGuess = $("#userGuess").val();
-    console.log("User Guess " + userGuess);
-  });
+
+
+
 });
 
 
